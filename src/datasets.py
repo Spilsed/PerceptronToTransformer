94d8b11172dda1037873369ef6d059cea4da2b8a
Spilsed <93678295+Spilsed@users.noreply.github.com>
2025-10-26 12:12:23 -0400

    Add seed to generate_blobs

--- src/datasets.py
+++ src/datasets.py
@@ -7,14 +7,15 @@ from sklearn.datasets import make_blobs
 from typing import Tuple
 
 
-def generate_blobs(n_samples: int = 1000, n_features: int = 2, centers: int = 2, cluster_std: int = 3, test_size: float = 0.2, one_hot: bool = True) -> Tuple[npt.NDArray, npt.NDArray, npt.NDArray, npt.NDArray]:
+def generate_blobs(n_samples: int = 1000, n_features: int = 2, centers: int = 2, cluster_std: int = 3, seed: int | None = None, test_size: float = 0.2, one_hot: bool = True) -> Tuple[npt.NDArray, npt.NDArray, npt.NDArray, npt.NDArray]:
     x, y = make_blobs(n_samples=n_samples,
                       n_features=n_features,
                       centers=centers,
-                      cluster_std=cluster_std)[:2]
+                      cluster_std=cluster_std,
+                      random_state=seed)[:2]
 
     x_train, x_test, y_train, y_test = train_test_split(
-        x, y, test_size=test_size, shuffle=True)
+        x, y, test_size=test_size, shuffle=True, random_state=seed)
 
     # Standardize the data to have a zero mean and unit variance
     scaler = StandardScaler()
